8c1177a5a470471d723a7fd0b42a62b7367ddc48
joleen tanous <116711100+joleentanous@users.noreply.github.com>
2024-07-17 16:45:48 +0300

    Update message_sender.c

--- message_sender.c
+++ message_sender.c
@@ -5,24 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "message_slot.h"
-
-
-/*
-Command line arguments:
-• argv[1]: message slot file path.
-• argv[2]: the target message channel id. Assume a non-negative integer.
-• argv[3]: the message to pass.
-
-
-
-static int device_open(struct inode *inode, struct file *file) {
-static long device_ioctl(struct file *file, unsigned int cmd_id, unsigned long arg) {
-static ssize_t device_write(struct file *file, const char __user *buffer, size_t length, loff_t *offset) {
-*/
-
 
 int main(int argc, char const *argv[]) {
     int file_open, chID_set, bytes_num, msg_size;
@@ -38,6 +24,7 @@ int main(int argc, char const *argv[]) {
     file_path = argv[1];
     channel_id = atoi(argv[2]);
     message = argv[3];
+    msg_size = strlen(message);
 
     file_open = open(file_path, O_WRONLY);
     if (file_open < 0){
@@ -46,14 +33,16 @@ int main(int argc, char const *argv[]) {
     }
 
     chID_set = ioctl(file_open, MSG_SLOT_CHANNEL, &channel_id);
-    if (chID_set !=0 ){
+    if (chID_set != 0){
+        close(file_open);
         perror("setting channel id to the id you provided has failed");
         exit(1);
     }
 
-    msg_size = strlen(message);
+    
     bytes_num = write(file_open, message, msg_size);
     if (bytes_num < 0){
+        close(file_open);
         perror("writing the message has failed");
         exit(1);
     }

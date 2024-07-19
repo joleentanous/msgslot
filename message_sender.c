9cee16bec50a7e824cd980efaf030bfebb65b5ff
joleen tanous <116711100+joleentanous@users.noreply.github.com>
2024-07-19 12:58:58 +0300

    Update message_sender.c

--- message_sender.c
+++ message_sender.c
@@ -10,49 +10,54 @@
 
 #include "message_slot.h"
 
-int main(int argc, char const *argv[]) {
-    int file_open, chID_set, bytes_num, msg_size;
-    unsigned int channel_id;
-    const char* file_path;
-    const char *message;
 
-    if (argc != 4){
+int main(int argc, char *argv[]) {
+    int file_open;
+    long chID_set;
+    unsigned int channel_id;
+    ssize_t bytes_num;
+    size_t msg_size;
+
+    channel_id = atoi(argv[2]);
+    msg_size = strlen(argv[3]);
+
+    if (argc != 4) {
         perror("invalid number of arguements provided!");
         exit(1);
     }
-    
-    file_path = argv[1];
-    channel_id = atoi(argv[2]);
-    message = argv[3];
-    msg_size = strlen(message);
 
-    printf("before file open\n");
-    file_open = open(file_path, O_RDWR);
-    printf("after file open\n");
-    if (file_open < 0){
+    file_open = open(argv[1], O_RDWR);
+    if (file_open < 0) {
         perror("opening the device file has failed");
         exit(1);
     }
 
-    printf("before ioctl\n");
-    chID_set = ioctl(file_open, MSG_SLOT_CHANNEL, &channel_id);
-    printf("after ioctl\n");
-    if (chID_set != 0){
+    if (channel_id == 0) {
+        fprintf(stderr, "Invalid channel ID\n");
         close(file_open);
-        perror("setting channel id to the id you provided has failed");
         exit(1);
     }
 
-    printf("before write\n");
-    bytes_num = write(file_open, message, msg_size);
-    printf("after write\n");
-    if (bytes_num != msg_size){
+    chID_set = ioctl(file_open, MSG_SLOT_CHANNEL, channel_id);
+    if (chID_set < 0){
+        perror("setting channel id to the id you provided has failed");
         close(file_open);
+        exit(1);
+    }
+
+    if (msg_size == 0 || msg_size > MAX_MSG_LEN) {
+        fprintf(stderr, "Message length is invalid\n");
+        close(file_open);
+        exit(1);
+    }
+
+    bytes_num = write(file_open, argv[3], msg_size);
+    if (bytes_num < 0) {
         perror("writing the message has failed");
+        close(file_open);
         exit(1);
     }
 
     close(file_open);
     return SUCCESS;
-
 }

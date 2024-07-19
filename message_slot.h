eac3df151698ad91059f49c08f9dd38acdee2e62
joleen tanous <116711100+joleentanous@users.noreply.github.com>
2024-07-19 12:57:49 +0300

    Update message_slot.h

--- message_slot.h
+++ message_slot.h
@@ -9,56 +9,19 @@
 #define SUCCESS 0
 #define MSG_SLOT_CHANNEL _IOW(MJR, 0, unsigned int)
 
-
-
-typedef struct channel {
-    unsigned long ch_id;
-    char *message;
-    int message_length;
-    struct channel *next_ch;
+typedef struct channel_s {
+    long message_length;
+    char message[MAX_MSG_LEN];
+    unsigned int ch_id;
+    struct channel_s *next_ch;
 } channel;
 
-typedef struct file_data {
-    int minor;
+typedef struct file_data_s {
+    unsigned int minor;
     channel *channels_head;
     channel *working_channel;
 } file_data;
 
 
-/*
-typedef struct channel_s {
-    unsigned long id;
-    char message[MAX_MSG_LEN];
-    int message_length;
-    struct channel *next;
-} channel;
-
-
-typedef struct device_f {
-	int minor;
-	device_f *next_device;
-	channel **channels;
-} device_f;
-
-*/
-
-
-
-/*
-struct message_slot_channel_s {
-    char *message;
-    size_t message_length;
-} message_slot_channel;
-
-typedef struct message_slot_s {
-    channel_t *channels;
-} message_slot;
-
-*/
-
-
-
-
-
-
 #endif
+        
